8cb12946cdb3d32583f7df81e9a40e6c00d949cb
hanichris <hanichris71@gmail.com>
2023-06-25 23:42:02 +0000

    Change the entry of the config to include different entry points. Provide configuration to setup a development server as well as clean up the output directory.

---
+++ task_3/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    header: path.resolve(__dirname, 'header/header.js'),
+    body: path.resolve(__dirname, 'body/body.js'),
+    footer: path.resolve(__dirname, 'footer/footer.js'),
+  },
+  // devtool: 'inline-source-map',
+  plugins: [new HtmlWebpackPlugin()],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+    publicPath: '/',
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  devServer: {
+    static: './public',
+    open: true,
+    port: 8564,
+    onListening: function (devServer) {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined.');
+      }
+      const port = devServer.server.address().port;
+      console.log(`listening on port: ${port}`);
+    }
+  },
+  performance: {
+    assetFilter: function (assetFilename) {
+      return assetFilename.endsWith('.js');
+    },
+    hints: 'warning',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: 'style-loader'},
+          { loader: 'css-loader'},
+        ],
+      },
+      {
+        test: /\.(?:png|ico|gif|jpe?g)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
