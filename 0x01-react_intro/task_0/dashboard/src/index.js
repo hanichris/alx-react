f25d672020fe6693f1b8bbbf5b3c45130f268cc5
hanichris <hanichris71@gmail.com>
2023-07-16 20:32:15 +0000

    Include a new Notifications Component int the render.

--- 0x01-react_intro/task_0/dashboard/src/index.js
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div id='root_notifications'>
+      <Notifications />
+    </div>
     <App />
   </React.StrictMode>
 );
