46bd3b4a4562544dea681f49ab80673d8f3941c8
hanichris <hanichris71@gmail.com>
2023-07-16 20:31:32 +0000

    Create a label and input for both email and password Include a button with the text `OK` at the end.

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -12,6 +12,11 @@ function App() {
     <hr/>
     <div className='App-body'>
       <p>Login to access the full dashboard</p>
+      <label htmlFor='user_email'>Email: </label>
+      <input type='email' id='user_email' name='user_email' />
+      <label htmlFor='user_pwd'>Password: </label>
+      <input type='password' id='user_pwd' name='user_pwd'/>
+      <button type='submit'>OK</button>
     </div>
     <hr/>
     <div className='App-footer'>
