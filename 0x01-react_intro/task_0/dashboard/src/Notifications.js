ff7d748312748f79113dc76d655476a408612dcd
hanichris <hanichris71@gmail.com>
2023-07-16 20:33:22 +0000

    Style the button for the notifications `div`using inline styling. Add an event listener for the button.

--- 0x01-react_intro/task_0/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_0/dashboard/src/Notifications.js
@@ -1,9 +1,27 @@
 import { React } from 'react';
 import './Notifications.css';
+// import close_png from './close.png'
 
 export default function Notifications () {
   return (
   <div className='Notifications'>
+    <button
+    style={{
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      border: 'none',
+      padding: '0.5rem',
+      lineHeight: '1rem',
+      fontSize: '1.5rem',
+      backgroundColor: 'transparent',
+      cursor: 'pointer',
+      color: 'darkslategray'
+    }}
+    aria-label='Close'
+    onClick={e => console.log('Close button has been clicked')}>
+      x
+    </button>
     <p>Here is the list of Notifications</p>
   </div>
   );
